1e29c5c9c51e20447a221f508a1fedef24b7e6ca
Tommy <rossithomashernan@gmail.com>
2025-12-11 18:09:26 -0300

    fix

--- models/User.js
+++ models/User.js
@@ -30,20 +30,25 @@ const UserSchema = new mongoose.Schema({
 
 // 4. Middleware: Cifrar la Contraseña antes de guardarla
 // Este código se ejecuta SIEMPRE antes de que Mongoose guarde o actualice un usuario
-UserSchema.pre('save', async function (next) {
+UserSchema.pre('save', function (next) { // <--- ELIMINAMOS 'async' DE AQUÍ
+    const user = this;
+
     // Si la contraseña no fue modificada, pasa al siguiente middleware/guardado
-    if (!this.isModified('password')) {
+    if (!user.isModified('password')) {
         return next();
     }
     
-    // 1. Generar el 'salt' (el factor aleatorio para el hash)
-    const salt = await bcrypt.genSalt(10); 
-    
-    // 2. Hashear la contraseña usando el salt
-    this.password = await bcrypt.hash(this.password, salt);
-    
-    // Continúa con la operación de guardado
-    next();
+    // Usamos bcrypt.genSalt/hash con callbacks/promesas, que es más seguro en este hook
+    bcrypt.genSalt(10, (err, salt) => {
+        if (err) return next(err); // Si hay error en salt, lo pasamos a Mongoose
+        
+        bcrypt.hash(user.password, salt, (err, hash) => {
+            if (err) return next(err); // Si hay error en hash, lo pasamos a Mongoose
+            
+            user.password = hash;
+            next(); // Continuamos la operación SÓLO cuando el hash ha terminado
+        });
+    });
 });
 
 module.exports = mongoose.model('User', UserSchema);
